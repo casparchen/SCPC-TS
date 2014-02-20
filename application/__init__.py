71a5cad13bd0e0f1a1b85655411e915b96a7daa6
Yi Chen <caspar.yi.chen@gmail.com>
2014-02-20 23:36:27 +0800

    3

--- application/__init__.py
+++ application/__init__.py
@@ -4,7 +4,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object('config')
 
-db = SQLAlchemy(app)
-db
 
-from application import views
+db = SQLAlchemy(app)
+
+from application import views 

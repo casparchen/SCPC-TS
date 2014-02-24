0adbdd2441cf9336789cba34fa4c95e491e6d10c
Yi Chen <caspar.yi.chen@gmail.com>
2014-02-24 13:52:37 +0800

    update plugin list

--- config.py
+++ config.py
@@ -3,6 +3,7 @@
 # database
 SQLALCHEMY_DATABASE_URI = 'mysql://root:scpc@mrchenyi.com/SCPC_TS?charset=utf8'
 #SQLALCHEMY_ECHO = True
+CONNECTION_ARGS = {'pool_recycle':3600}
 
 # site name
 SCPC_TS_SITE_NAME = 'SCPC Training System V2'
